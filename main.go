1dcc535e4369b035a7d782c871c6951980c2b8a0
tuannabolas <dms.sys.dev@gmail.com>
2024-04-05 16:28:13 +0700

    to make easy to test each of answer

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+please uncomment one of each line to do test.
+thanks a lot for chance that been given to me to follow this recruitment process
+*/
+
+import (
+	//numfive "pranala/test/num_five"
+	//numfour "pranala/test/num_four"
+	numthree "pranala/test/num_three"
+)
+
+func main() {
+	numthree.NumberThree()
+	//numfour.NumFour()
+	//numfive.NumFive()
+
+}
